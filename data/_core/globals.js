5e3a7f4694a26731cf78dc30d1e27fd041836c65
DevOps <devops@greenminds.ru>
2019-10-23 11:03:14 +0300

    Define `globals`

--- data/_core/globals.js
+++ data/_core/globals.js
@@ -1,1 +1,21 @@
-module.exports = {};
+const blogName = 'Culinary stories';
+const blogDesc = 'Tasteful in life: culinary stories, tales and novels';
+const socials = {
+  twitter: 'https://twitter.com',
+  facebook: 'https://facebook.com',
+  instagram: 'https://www.instagram.com',
+};
+const contacts = {
+  address: '203 Fake St. Mountain View, San Francisco, California, USA',
+  phone: '+2 392 3929 210',
+  email: 'info@yourdomain.com',
+};
+const currentYear = (new Date()).getFullYear();
+
+module.exports = {
+  blogName,
+  blogDesc,
+  socials,
+  contacts,
+  currentYear,
+};
